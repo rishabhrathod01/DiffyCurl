b6397abc34a0c687d99055fb43f72b97aceee2d7
Rishabh-Rathod <rishabhrathod2012@gmail.com>
2020-07-26 04:10:38 +0530

    Updates

---
+++ precache-manifest.708539300ed0dd6a29953038aa37a7c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d24d522d567f5b4c8114ecebcd12d47a",
+    "url": "/diffycurl/index.html"
+  },
+  {
+    "revision": "b3d1f82ece2a28ebe036",
+    "url": "/diffycurl/static/css/main.ba8a2f50.chunk.css"
+  },
+  {
+    "revision": "8c908512287c3383edf4",
+    "url": "/diffycurl/static/js/2.53c20fb2.chunk.js"
+  },
+  {
+    "revision": "a564d5a151aad9a6f62162799eed14f3",
+    "url": "/diffycurl/static/js/2.53c20fb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3d1f82ece2a28ebe036",
+    "url": "/diffycurl/static/js/main.eeaf641c.chunk.js"
+  },
+  {
+    "revision": "013ed8f59d4293708481",
+    "url": "/diffycurl/static/js/runtime-main.ad3eb1ed.js"
+  }
+]);
